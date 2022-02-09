66e7eba255e5f3a51593f1e98fdc0a7f1515c0f6
nacukisubaru <alexitsheff@gmail.com>
2022-02-09 10:12:15 +0300

    работа с классом Store и updateState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import Counter from "./components/Counter"
+import Counter from "./components/Counter";
+import Store from "./redux/store";
 
+//чистая функция так как передаем два параметра и работаем только с ними
+//неизменность данных так как на выходе мы вернем новый объект а не изменим старый
+// function updateState(state, action) {
+//     if (action.type === "INCREMENT") {
+//         return { count: state.count + action.amount };
+//     } else if (action.type === "DECREMENT") {
+//         return { count: state.count - action.amount };
+//     } else {
+//         return state;
+//     }
+// }
 
 function App() {
-  return (
-    <div className="App">
-     {<Counter />}
-    </div>
-  );
+    // const initialState = {count: 0}
+    // const store = new Store(updateState, initialState)
+
+    // const unsubscribe = store.subscribe(() => console.log('State changed', store.state))
+    // store.subscribe(() => console.log('State changed', store.state))
+    // store.update({type: 'INCREMENT', amount: 5})
+    // //unsubscribe()
+    // store.update({type: 'DECREMENT', amount: 2})
+
+    return <div className="App">{<Counter />}</div>;
 }
 
 export default App;
